23ec68304365b39ca51e36aefc96f18195b940cb
ganesh khatri <kh6ganesh@gmail.com>
2020-12-23 10:32:35 +0545

    old questions solutions update

---
+++ labcode/php/user.php
@@ -0,0 +1,10 @@
+
+
+<form method="POST" action="">
+	Name : <input type="text" name="name"><br><br>
+	Email : <input type="email" name="email"><br><br>
+	Phone : <input type="number" name="phone"><br><br>
+	Gender : <input type="radio" name="gender" value="Male"> Male 
+			   <input type="radio" name="gender" value="Female"> Female 
+	<input type="submit" name="save" value="Save">
+</form>
